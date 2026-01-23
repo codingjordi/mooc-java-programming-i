2300f71e54074e4d0041bd1aeb2780eda126755c
Jordi Saperas Castro <jordisaperasc@gmail.com>
2026-01-23 18:45:46 +0100

    Exercise Part04_01.YourFirstAccount

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,14 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account cuentaJordi = new Account("Jordi", 100);
+        
+        cuentaJordi.deposit(20);
+        
+        System.out.println(cuentaJordi);
+    }
+}

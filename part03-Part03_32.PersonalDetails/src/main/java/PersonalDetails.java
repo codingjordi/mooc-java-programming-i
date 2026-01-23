b3490b0ae9c56126076680cad2509d09812e2387
Jordi Saperas Castro <jordisaperasc@gmail.com>
2026-01-23 14:19:48 +0100

    Exercise part03-Part03_32.PersonalDetails

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,38 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int acc = 0;
+        String longest = "";
+        int persons = 0;
+        
+        
+        while(true){
+            //1.demanar dadesm(nom,edat)
+        String csv = scanner.nextLine();
+        
+        //2. break si cadena vuida
+        if(csv.isEmpty()){
+            break;
+        }
+                
+        // pasar csv a array de string
+        String[] details = csv.split(",");
+        
+        //3. sumar edat personal al acc
+        persons++;
+        acc += Integer.valueOf(details[1]);
+        
+        
+        //4. comparar si nom es mes llarg que longest
+        if(longest.length() < details[0].length()){
+              longest = details[0];
+            }
+        }
+        
+        System.out.println("Longest name: " + longest);
+        System.out.println("Average of the birth years: " + (double)acc / (double)persons);
+        
 
     }
 }

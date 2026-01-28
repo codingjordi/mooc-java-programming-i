bb1873332858f0729e03cf77a7ba8e037d3c2096
Jordi Saperas Castro <jordisaperasc@gmail.com>
2026-01-28 21:45:00 +0100

    Exercise Part05_07.NullPointerException

---
+++ part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class NullPointerExceptionProgram {
+
+    public static void main(String[] args) {
+        // Implement a program, or modify a program so, that it causes 
+        // the NullPointerException -error
+
+        String x = null;
+        x.concat("o");
+    }
+}
+    
+

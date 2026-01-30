374c231c6da9bb2aaaabb8128f42c9390966b7e2
Jordi Saperas Castro <jordisaperasc@gmail.com>
2026-01-30 18:41:05 +0100

    Exercise Part05_15.Archive

---
+++ part05-Part05_15.Archive/src/main/java/Item.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author josaca
+ */
+public class Item {
+    
+    private String identifier;
+    private String name;
+    
+    public Item(String identifier, String name) {
+        this.identifier = identifier;
+        this.name = name;
+    }
+    
+    public String toString() {
+        return this.identifier + ": " + this.name;
+    } 
+    
+    @Override
+    public boolean equals(Object compared) {
+        
+        if(this == compared) {
+            return true;
+        }
+        
+        if(!(compared instanceof Item)){
+            return false;
+        }
+        
+        Item itemCompared = (Item) compared;
+        
+        return itemCompared.identifier.equals(this.identifier);
+    }
+    
+}
